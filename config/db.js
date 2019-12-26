18f8b629354aa7c224ef5fe3d6a2ac2c4de230e5
Shihao Wen <wenshihao22@gmail.com>
2019-12-26 17:33:53 -0500

    MongoDB async version

--- config/db.js
+++ config/db.js
@@ -2,20 +2,34 @@ const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
 
-const connectDB = () => {
-  mongoose
-    .connect(db, {
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
       useNewUrlParser: true,
       useCreateIndex: true,
-      useFindAndModify: false
-    })
-    .then(() => {
-      console.log('MongoDB Connected');
-    })
-    .catch(err => {
-      console.err(err.message);
-      process.exit(1);
+      useFindAndModify: false,
+      useUnifiedTopology: true
     });
+    console.log('MongoDB Connected');
+  } catch (err) {
+    console.err(err.message);
+    process.exit(1);
+  }
 };
+// const connectDB = () => {
+//   mongoose
+//     .connect(db, {
+//       useNewUrlParser: true,
+//       useCreateIndex: true,
+//       useFindAndModify: false
+//     })
+//     .then(() => {
+//       console.log('MongoDB Connected');
+//     })
+//     .catch(err => {
+//       console.err(err.message);
+//       process.exit(1);
+//     });
+// };
 
 module.exports = connectDB;
