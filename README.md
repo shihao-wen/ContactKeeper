bf592e3ea6a31d6ec6682b6d888b0ff9139ff47b
Shihao Wen <wenshihao22@gmail.com>
2019-12-26 16:57:41 -0500

    setup routes

---
+++ README.md
@@ -0,0 +1,10 @@
+# Contact Keeper
+
+- Set up Dependencies
+
+  - npm install express bcryptjs jsonwebtoken config express-validator mongoose
+  - npm install -D nodeman concurrently
+
+- Set up Express Server
+  - set up route (users, contacts, auth)
+- Set up MongoDB
---
+++ routes/auth.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+// @route       Get api/auth
+// @desc        Get logged in user
+// @access      Private
+router.get('/', (req, res) => {
+  res.send('Get logged in user.');
+});
+
+// @route       Post api/auth
+// @desc        Auth user & get Token
+// @access      Public
+router.post('/', (req, res) => {
+  res.send('Login User.');
+});
+
+module.exports = router;
