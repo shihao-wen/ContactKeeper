70d7bc455660484b8aa9217404d441520ea37626
Shihao Wen <wenshihao22@gmail.com>
2019-12-27 16:01:35 -0500

    Prepare for Deployment

--- server.js
+++ server.js
@@ -1,22 +1,30 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
+
 const app = express();
-const PORT = process.env.PORT || 5000;
+
 // Connect Database
 connectDB();
 
-// Init MiddleWare
+// Init Middleware
 app.use(express.json({ extended: false }));
-
-app.get('/', (req, res) => {
-  res.json({ msg: 'Welcome to the contact keeper api' });
-});
 
 // Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
-app.listen(PORT, () => {
-  console.log(`Server Started on Port ${PORT}`);
-});
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
